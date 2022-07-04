ce4b7ca9c18e66040eadbae4e4c47c8e4f1ec406
Ramon Pinho <ramonuchoa386@gmail.com>
2022-07-04 01:23:29 -0300

    feat: component improvements

--- src/components/listItem/styles.ts
+++ src/components/listItem/styles.ts
@@ -1,17 +1,11 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-export const ThumbnailWrapper = styled.section`
-	width: 144px;
-	height: 144px;
-	overflow: hidden;
-	border-bottom: 3px solid red;
-	margin-bottom: 8px;
-`;
-
 export const HeroThumbnail = styled.img`
 	width: 100%;
 	height: auto;
+	border-bottom: 3px solid red;
+	margin-bottom: 8px;
 `;
 
 export const HeroNameWrapper = styled.section`
@@ -24,6 +18,10 @@ export const HeroNameWrapper = styled.section`
 export const ItemWrapper = styled.li`
 	width: 45%;
 	margin-bottom: 16px;
+
+	@media only screen and (min-width: 768px) {
+		max-width: 220px;
+	}
 `;
 
 export const LinkToHero = styled(Link)`
