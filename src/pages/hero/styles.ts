9f0e35ee12801227f41623a1ba27d3925341abf8
Ramon Pinho <ramonuchoa386@gmail.com>
2022-07-04 01:24:01 -0300

    feat: hero fetching data and paginate

--- src/pages/hero/styles.ts
+++ src/pages/hero/styles.ts
@@ -26,3 +26,69 @@ export const HerosList = styled.ul`
 	margin: 0 auto;
 	padding: 0;
 `;
+
+export const HeroHeader = styled.header`
+	display: flex;
+	justify-content: space-between;
+	align-items: flex-start;
+	margin-bottom: 8px;
+`;
+
+export const HeroFeats = styled.ul`
+	list-style-type: none;
+	display: flex;
+	justify-content: space-between;
+	align-items: flex-start;
+	margin-top: 16px;
+
+	li {
+		width: 45%;
+
+		h3 {
+			margin-bottom: 8px;
+		}
+		div {
+			display: flex;
+			justify-content: space-between;
+			align-items: flex-start;
+		}
+	}
+`;
+
+export const HeroImages = styled.section`
+	position: relative;
+	margin-top: 54px;
+	height: 50vh;
+
+	img:first-of-type {
+		position: absolute;
+		top: 0px;
+		left: 20px;
+		opacity: 0.5;
+		z-index: 1;
+	}
+
+	img:last-of-type {
+		position: absolute;
+		top: 200px;
+		left: 50px;
+	}
+`;
+
+export const HeroInfoContainer = styled.div`
+	@media only screen and (min-width: 768px) {
+		display: flex;
+
+		& > section {
+			width: 50%;
+		}
+	}
+`;
+
+export const FlexContainer = styled.div`
+	@media only screen and (min-width: 768px) {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+`;
