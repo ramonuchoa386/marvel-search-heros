358f7d23c6d00444b000a934ee66e35401b2c65b
Ramon Pinho <ramonuchoa386@gmail.com>
2022-07-04 01:23:06 -0300

    feat: setting container breakoints

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -2,6 +2,16 @@ import styled from 'styled-components';
 
 const Container = styled.section`
 	padding: 20px 20px 0;
+
+	@media only screen and (min-width: 768px) {
+		padding-left: 56px;
+		padding-right: 56px;
+	}
+
+	@media only screen and (min-width: 1280px) {
+		padding-left: 130px;
+		padding-right: 130px;
+	}
 `;
 
 export default Container;
